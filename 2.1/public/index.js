fa246f9831320fc7e5ed92875ebf9cf6fee0201f
ana.araujo <ana.araujo@student.vinci.be>
2023-11-25 20:18:37 +0100

    2.1 : HMI : first external script.

---
+++ 2.1/public/index.js
@@ -0,0 +1,7 @@
+const dateTimeNow = new Date();
+
+console.log(dateTimeNow.toLocaleDateString()); 
+
+console.log(dateTimeNow.toLocaleTimeString());
+
+alert(dateTimeNow.toLocaleDateString() + " "  + dateTimeNow.toLocaleTimeString() +  " This is the best moment to have a look at this website !");
