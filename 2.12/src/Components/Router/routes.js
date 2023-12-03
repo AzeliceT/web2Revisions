4f41a9332258d02e7224ae073613e1da2deaf133
ana.araujo <ana.araujo@student.vinci.be>
2023-12-04 00:08:26 +0100

    2.12 : dynamic page content.

---
+++ 2.12/src/Components/Router/routes.js
@@ -0,0 +1,13 @@
+import HomePage from '../Pages/HomePage';
+import AddMoviePage from '../Pages/AddMoviePage';
+import ViewMoviePage from '../Pages/ViewMoviePage';
+import AboutPage from '../Pages/AboutPage';
+
+const routes = {
+  '/': HomePage,
+  '/addMovie': AddMoviePage,
+  '/viewMovie': ViewMoviePage,
+  '/about': AboutPage,
+};
+
+export default routes;
