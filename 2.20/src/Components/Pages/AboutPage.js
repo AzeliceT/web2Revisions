a51e8d64405fabe6ad446d7acaf8d19b3a80e559
ana.araujo <ana.araujo@student.vinci.be>
2023-12-09 11:35:39 +0100

    user session & theme & user preferences

---
+++ 2.20/src/Components/Pages/AboutPage.js
@@ -0,0 +1,14 @@
+const AboutPage = () => {
+    const main = document.querySelector('main');
+    main.innerHTML = `
+    <div class="container text-center">
+            
+                <h4>About CREATOR </h4>
+  
+                <p>TEST 2 2 22 2 2 2 2 2 2 22 2 2 </p>
+              
+                 
+    </div>`;
+};
+
+export default AboutPage;
---
+++ 2.20/src/Components/Router/Router.js
@@ -0,0 +1,46 @@
+import routes from './routes';
+
+const Router = () => {
+  onFrontendLoad();
+  // gère quand il y a un chargement du frontend (qd on fait un refresh ou bien la toute 1ère fois où on ouvre l'application)
+  onNavBarClick();
+  // gère les clicks sur la navbar 
+  onHistoryChange();
+  // quand on clique sur la flèche du browser à gauche ou à droite
+};
+
+function onNavBarClick() {
+  const navItems = document.querySelectorAll('.nav-link');
+
+  navItems.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      const uri = e.target?.dataset?.uri;
+      const componentToRender = routes[uri];
+      if (!componentToRender) throw Error(`The ${uri} ressource does not exist.`);
+
+      componentToRender();
+      window.history.pushState({}, '', uri);
+    });
+  });
+}
+
+function onHistoryChange() {
+  window.addEventListener('popstate', () => {
+    const uri = window.location.pathname;
+    const componentToRender = routes[uri];
+    componentToRender();
+  });
+}
+
+function onFrontendLoad() {
+  window.addEventListener('load', () => {
+    const uri = window.location.pathname;
+    const componentToRender = routes[uri];
+    if (!componentToRender) throw Error(`The ${uri} ressource does not exist.`);
+
+    componentToRender();
+  });
+}
+
+export default Router;
